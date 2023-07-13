10aa306080c0aa7ec2c85cf1b06230e1ccb4c186
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-07-13 14:25:50 -0500

    commiting merged code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,70 +2,48 @@
 	import Table from './table.svelte';
 
 	let array = [
-		{ cityName: 'FirstEntered', minCustomer: 23, maxCustomer: 65, avgCookieSaleHour: 6.3 },
-		{ cityName: 'SecondEntered', minCustomer: 3, maxCustomer: 24, avgCookieSaleHour: 1.2 },
-		{ cityName: 'SecondEntered', minCustomer: 3, maxCustomer: 24, avgCookieSaleHour: 1.2 },
-		{ cityName: 'SecondEntered', minCustomer: 3, maxCustomer: 24, avgCookieSaleHour: 1.2 },
-		{ cityName: 'SecondEntered', minCustomer: 3, maxCustomer: 24, avgCookieSaleHour: 1.2 },
-	]
-
-	let questions = [
-		{
-			id: 1,
-			text: `Where did you go to school?`
-		},
-		{
-			id: 2,
-			text: `What is your mother's name?`
-		},
-		{
-			id: 3,
-			text: `What is another personal fact that an attacker could easily find with Google?`
-		}
 	];
 
-	let selected;
+	let cityName;
+	let minCustomer;
+	let maxCustomer;
+	let avgCookieSaleHour;
+	$: minCustomer = minCustomer > maxCustomer ? maxCustomer : minCustomer;
 
-	let answer = '';
-
-	function handleSubmit() {
-		alert(
-			`answered question ${selected.id} (${selected.text}) with "${answer}"`
-		);
+	function handleSubmit(e) {
+		array = array.filter((city) => city.cityName === cityName).length
+			? array.map((city) =>
+					city.cityName === cityName
+						? { cityName, minCustomer, maxCustomer, avgCookieSaleHour }
+						: city
+			  )
+			: [...array, { cityName, minCustomer, maxCustomer, avgCookieSaleHour }];
 	}
 
+	function handleScroll(event) {
+		event.target.value = parseFloat(event.target.value) + event.deltaY * 0.01;
+	}
 </script>
 
 <!-- <main> include this later -->
 
-	<!-- <Form /> Future Form -->
-	<form on:submit|preventDefault={handleSubmit}>
-		<select
-			bind:value={selected}
-			on:change={() => (answer = '')}
-		>
-			{#each questions as question}
-				<option value={question}>
-					{question.text}
-				</option>
-			{/each}
-		</select>
-	
-		<input bind:value={answer} />
-	
-		<button disabled={!answer} type="submit">
-			Submit
-		</button>
-	</form>
-	
-	<div class="container">
+<!-- <Form /> Future Form -->
+<form on:submit|preventDefault={handleSubmit}>
+	<input bind:value={cityName} />
+	<input type="number" min="0" step="0.1" bind:value={avgCookieSaleHour} on:wheel={handleScroll} />
+	<input type="number" min="0" step="0.1" bind:value={maxCustomer} on:wheel={handleScroll} />
+	<input type="number" min="0" step="0.1" bind:value={minCustomer} on:wheel={handleScroll} />
 
-		<h1>Table</h1>
+	<button disabled={!(minCustomer && maxCustomer && avgCookieSaleHour)} type="submit">
+		Submit
+	</button>
+</form>
 
-		<Table cityObjArr={array} />
-	
-	</div>
-	
+<div class="container">
+	<h1>Table</h1>
+
+	<Table cityObjArr={array} />
+</div>
 
 <style>
 	/* Your CSS code goes here */
